a3696a615cfe2d8dca861fa942b9926d4bf439a7
aminBSHR <amin.bashiri28@gmail.com>
2018-07-01 14:07:58 +0430

    add material font.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
+// modules
 import Vue from 'vue'
+// global css style
+import './helper/style/reset.css'
+import './helper/style/main.css'
+import './helper/style/global.css'
+// components
 import App from './App.vue'
 
 new Vue({
