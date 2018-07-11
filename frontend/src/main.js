66e37197583a4c8b52e6a8490354e6d1f5379b1a
ali <aliasgary73@gmail.com>
2018-07-11 16:24:13 +0430

    change reset.css with normalize.css

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 // modules
 import Vue from 'vue'
 // global css style
-import './helper/style/reset.css'
+import './helper/style/normalize.css'
 import './helper/style/main.css'
 import './helper/style/global.css'
 // components
